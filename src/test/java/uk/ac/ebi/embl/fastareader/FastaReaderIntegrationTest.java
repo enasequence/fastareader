56a89d01ebec700f0eaa886baa36b7ef1f6e3801
Iva Tutis <tutis@ebi.ac.uk>
2026-02-06 11:36:43 +0000

    added-chinese

--- src/test/java/uk/ac/ebi/embl/fastareader/FastaReaderIntegrationTest.java
+++ src/test/java/uk/ac/ebi/embl/fastareader/FastaReaderIntegrationTest.java
@@ -59,7 +59,7 @@ class FastaReaderIntegrationTest {
                     .equals(">MCHU - Calmodulin - Human, rabbit, bovine, rat, and chicken Zażółć gęślą jaźń — 日本語"));
             assertTrue(entry2.get()
                     .headerLine
-                    .equals("> ID2 | {\"description\":\"x\", \"molecule_type\":\"dna\", \"topology\":\"linear\"}"));
+                    .equals("> ID2 | {\"description\":\"这绝对是一段\u200B\u200B描述。\", \"molecule_type\":\"脱氧核糖核酸\", \"topology\":\"linear\"}"));
         }
     }
 
